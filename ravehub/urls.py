346130cd0a97969371e7e5111a4e99f0f3d922fb
Daniel Turner <lunartechfreek@googlemail.com>
2024-06-13 17:35:31 +0000

    Add test code for deployment

--- ravehub/urls.py
+++ ravehub/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from event.views import my_event
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('event/', my_event, name='event'),
 ]
