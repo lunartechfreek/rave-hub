1ace49a511eee44df2afa3505b4882c2fb736bd7
Daniel Turner <lunartechfreek@googlemail.com>
2024-06-13 18:49:58 +0000

    Add genre and artist models

--- event/models.py
+++ event/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Genre(models.Model):
+    """ A model for artist genres (managed by site owner). """
+    name = models.CharField(max_length=75, null=False, blank=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Artist(models.Model):
+    """ A model for each artist (managed by site owner). """
+    name = models.CharField(max_length=100, null=False, blank=False)
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
