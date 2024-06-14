1ee50df1888d20c33a875eeb2b3c95dfe51efaaa
Daniel Turner <lunartechfreek@googlemail.com>
2024-06-14 18:14:30 +0000

    Remove comment

--- event/admin.py
+++ event/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from .models import Genre, Artist, Festival
 
-# Register your models here.
 admin.site.register(Genre)
 admin.site.register(Artist)
 admin.site.register(Festival)
