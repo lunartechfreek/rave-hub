2060164d92c4255672bcfd6b8df3cd3f5f908593
Daniel Turner <lunartechfreek@googlemail.com>
2024-06-14 14:43:11 +0000

    Add meta class to order by date

--- event/models.py
+++ event/models.py
@@ -40,6 +40,9 @@ class Festival(models.Model):
     longitude = models.DecimalField(
         max_digits=9, decimal_places=6, null=False, blank=False)
 
+    class Meta:
+        ordering = ['date']
+
     def __str__(self):
         return self.name
 
