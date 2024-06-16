287268cb2b2404bade774bcebf1527c5d7d4fdfb
Daniel Turner <lunartechfreek@googlemail.com>
2024-06-16 12:42:45 +0000

    Add summernote to INSTALLED_APPS

--- ravehub/settings.py
+++ ravehub/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-lunartechfreek-ravehub-e8pdyh5fjpg.ws.codeinstitute-ide.net',
                 '.herokuapp.com']
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_summernote',
     'event',
 ]
 
