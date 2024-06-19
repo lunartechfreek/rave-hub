8628c8991c70ff1d7938c0ac0e34ffbf400b34d3
Daniel Turner <lunartechfreek@googlemail.com>
2024-06-19 23:31:07 +0000

    Add views for festival_list and festival_detail

--- event/views.py
+++ event/views.py
@@ -1,5 +1,6 @@
 from django.views import generic
 from django.utils import timezone
+from django.shortcuts import render, get_object_or_404
 from .models import Festival
 
 class FestivalList(generic.ListView):
@@ -9,4 +10,14 @@ class FestivalList(generic.ListView):
 
     def get_queryset(self):
         # Ensures festivals that are in the past are not displayed
-        return Festival.objects.filter(approved=True, date__gte=timezone.now().date())
+        return Festival.objects.filter(approved=True, date__gte=timezone.now().date()).order_by('date')
+
+def festival_list(request):
+    print("festival_list view called")
+    festivals = Festival.objects.filter(date__gte=timezone.now().date(), approved=True).order_by('date')
+    print(festivals)
+    return render(request, 'event/festival_list.html', {'festivals': festivals})
+
+def festival_detail(request, id):
+    festival = get_object_or_404(Festival, id=id)
+    return render(request, 'event/festival_detail.html', {'festival': festival})
