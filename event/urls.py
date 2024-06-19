2309e626d5f05b18842f80432c7b1ba4a2730df9
Daniel Turner <lunartechfreek@googlemail.com>
2024-06-19 23:32:11 +0000

    Add url paths for festival list and detail

--- event/urls.py
+++ event/urls.py
@@ -3,4 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.FestivalList.as_view(), name='home'),
+    path('festival/', views.festival_list, name='festival_list'),
+    path('festival/<int:id>/', views.festival_detail, name='festival_detail'),
 ]
