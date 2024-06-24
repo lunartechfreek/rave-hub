ce27efe72516d5fb0b0ac4ca4ebd82009d35f36b
Daniel Turner <lunartechfreek@googlemail.com>
2024-06-24 19:52:20 +0000

    Remove commented out code

--- event/forms.py
+++ event/forms.py
@@ -25,33 +25,6 @@ class FestivalForm(forms.ModelForm):
             'date': forms.DateInput(attrs={'type': 'date'}),
         }
 
-#         def __init__(self, *args, **kwargs):
-#             super().__init__(*args, **kwargs)
-#             self.helper = FormHelper()
-#             self.helper.form_method = 'post'
-#             self.helper.add_input(Submit('submit', 'Submit', css_class='btn-primary'))
-#             self.helper.form_class = 'form-horizontal'  # Example: Add a form class
-
-#             # Add CSS classes to specific fields (e.g., artists)
-#             self.fields['artists'].widget.attrs['class'] = 'artist-checkboxes'
-
-#             self.helper.layout = Layout(
-#                 Div(
-#                     Field('artists'), css_class="artist-checkboxes"
-#                 ),
-#                 Field('description'),
-
-#             Field('artists', css_class="artist-checkboxes")
-        
-# )
-
-        
-
-    # def __init__(self, *args, **kwargs):
-    #     super(FestivalForm, self).__init__(*args, **kwargs)
-    #     self.fields['artists'].queryset = Artist.objects.all()
-    #     self.fields['artists'].widget = forms.CheckboxSelectMultiple()
-
     def clean_website(self):
         website = self.cleaned_data.get('website')
         if website and not website.startswith(('http://', 'https://')):
