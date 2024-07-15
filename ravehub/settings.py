777312c8d06d3d8bd36980c0504f510aab94c156
Daniel Turner <lunartechfreek@googlemail.com>
2024-07-15 18:46:02 +0000

    Update allowed hosts

--- ravehub/settings.py
+++ ravehub/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-lunartechfreek-ravehub-e8pdyh5fjpg.ws.codeinstitute-ide.net',
+ALLOWED_HOSTS = ['8000-lunartechfreek-ravehub-ahsesvrpz3p.ws.codeinstitute-ide.net',
                 '.herokuapp.com']
 
 
