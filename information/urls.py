d460ee4f689b9801a62f1f54a9e02c50f46a35d2
Daniel Turner <lunartechfreek@googlemail.com>
2024-08-03 18:20:41 +0000

    Add comments

--- information/urls.py
+++ information/urls.py
@@ -1,6 +1,7 @@
 from . import views
 from django.urls import path
 
+# Url patterns for the information app
 urlpatterns = [
     path('about', views.about_us, name='about'),
     path('contact-us/', views.contact_us, name='contact_us'),
